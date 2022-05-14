e2e1d97cdb4534daa085e0813253a5830797b573
wjunin1 <wpsjunior23@gmail.com>
2022-05-14 12:52:53 -0300

    dragDrop hooks describe

--- cypress/integration/dragAndDrop.spec.js
+++ cypress/integration/dragAndDrop.spec.js
@@ -1,17 +1,18 @@
-it("deve arrastar o cypress para a caixinha do node", () => {
-  cy.Entrar("Drag and Drop");
+describe("", () => {
+  it("deve arrastar o cypress para a caixinha do node", () => {
+    cy.Entrar("Drag and Drop");
 
-  const dataTransfer = new DataTransfer();
+    const dataTransfer = new DataTransfer();
 
-  cy.get("img[alt=Cypress]").trigger("dragstart", { dataTransfer });
-  cy.get(".nodejs figure[draggable=true]").trigger("drop", { dataTransfer });
-});
+    cy.get("img[alt=Cypress]").trigger("dragstart", { dataTransfer });
+    cy.get(".nodejs figure[draggable=true]").trigger("drop", { dataTransfer });
+  });
 
-it("deve arrastar o robot para a caixinha do phyton", () => {
-  cy.Entrar("Drag and Drop");
+  it("deve arrastar o robot para a caixinha do phyton", () => {
 
-  const dataTransfer = new DataTransfer();
+    const dataTransfer = new DataTransfer();
 
-  cy.get("img[alt='Robot Framework']").trigger("dragstart", { dataTransfer });
-  cy.get(".python figure[draggable=true]").trigger("drop", { dataTransfer });
+    cy.get("img[alt='Robot Framework']").trigger("dragstart", { dataTransfer });
+    cy.get(".python figure[draggable=true]").trigger("drop", { dataTransfer });
+  });
 });
