c8667d0490ea31689d21e4be64cde137906e8c17
wjunin1 <wpsjunior23@gmail.com>
2022-05-14 11:29:35 -0300

    dragAndDrop

---
+++ cypress/integration/dragAndDrop.spec.js
@@ -0,0 +1,17 @@
+it("deve arrastar o cypress para a caixinha do node", () => {
+  cy.Entrar("Drag and Drop");
+
+  const dataTransfer = new DataTransfer();
+
+  cy.get("img[alt=Cypress]").trigger("dragstart", { dataTransfer });
+  cy.get(".nodejs figure[draggable=true]").trigger("drop", { dataTransfer });
+});
+
+it("deve arrastar o robot para a caixinha do phyton", () => {
+  cy.Entrar("Drag and Drop");
+
+  const dataTransfer = new DataTransfer();
+
+  cy.get("img[alt='Robot Framework']").trigger("dragstart", { dataTransfer });
+  cy.get(".python figure[draggable=true]").trigger("drop", { dataTransfer });
+});
