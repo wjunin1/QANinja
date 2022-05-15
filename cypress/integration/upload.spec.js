322113fb7482dd3b5ab4f468a193d207aa8ea6b0
wjunin1 <wpsjunior23@gmail.com>
2022-05-14 21:22:21 -0300

    upload

--- cypress/integration/upload.spec.js
+++ cypress/integration/upload.spec.js
@@ -1,4 +1,11 @@
-it('Enviar arquivo', () => {
-    cy.Entrar('Upload de imagem');
+it("Enviar arquivo", () => {
+  cy.Entrar("Upload de imagem");
 
+  const imageFile = "cypress/fixtures/wallpaper.jpg";
+
+  cy.get("#file-upload").selectFile(imageFile, { force: true });
+  cy.get("#file-submit").click();
+  cy.get("img[src='/uploads/wallpaper.jpg']", { timeout: 7000 }).should(
+    "be.visible"
+  );
 });
